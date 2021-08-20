91f3a7a8478bb800ac35d84f396ea51f1c71dd9e
user <user@users-MacBook-Pro.local>
2021-08-19 19:54:37 -0700

    created alert message to open each time page load using js. working on creating tab icon with dashboard and will create dashboard traffic after completeing dashboard icon

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+const alertDiv = document.querySelector('.alert');
+
+function alertMessage() {
+  alertDiv.innerHTML = `<h2><span class='bold'>Alert</span>: You have unread messages</h2><span class='close'>&times;</span>`;
+}
+
+window.onload = alertMessage();
+
+alertDiv.addEventListener('click', (e)=> {
+  if(e.target.tagName === 'SPAN') {
+    if(e.target.className === 'close') {
+      let h2 = e.target.parentNode;
+      let alert = h2.parentNode;
+      alert.removeChild(h2);
+    }
+  }
+})
