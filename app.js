7c579968a1015e4b74346b9b5a4e131df88a7d5f
user <user@users-MacBook-Pro.local>
2021-08-20 20:09:32 -0700

    finished dashboard tab element and add a min-width to svg icon within user-info class. Working on creating charts for traffic, daily traffic, and mobile-users

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
 const alertDiv = document.querySelector('.alert');
+let ctxLine = document.getElementById('lineChart');
+let ctxBar = document.getElementById('barChart');
+let ctxPie = document.getElementById('pieChart');
+let stars = [135850, 52122, 148825, 16939, 9763];
+let frameworks = ['React', 'Angular', 'Vue', 'Hyperapp', 'Omi'];
 
 function alertMessage() {
   alertDiv.innerHTML = `<h2><span class='bold'>Alert</span>: You have unread messages</h2><span class='close'>&times;</span>`;
 }
 
 window.onload = alertMessage();
+
+const barChart = new Chart(ctxBar, {
+  type: 'bar',
+  data: {
+     labels: frameworks,
+     datasets: [{
+         label: 'Popular JavaScript Frameworks',
+         data: stars,
+         backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)"
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            ],
+            borderWidth: 1
+         }]
+  }
+})
 
 alertDiv.addEventListener('click', (e)=> {
   if(e.target.tagName === 'SPAN') {
